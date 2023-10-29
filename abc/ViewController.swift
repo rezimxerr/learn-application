dfcb660b86bf67fabb504c1516aec2837a7b3224
Khakim <luthfiel99@gmail.com>
2023-10-29 08:13:17 +0700

    Initial Commit

---
+++ abc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by Khakim on 29/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
